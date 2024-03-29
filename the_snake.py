a3102e395f2dc2f3eea7941048c7440b3c5c0b4e
y353x <xyuhe9@gmail.com>
2024-03-29 13:32:23 +0300

    edit classes (Apple)

--- the_snake.py
+++ the_snake.py
@@ -43,7 +43,39 @@ clock = pygame.time.Clock()
 
 
 # Тут опишите все классы игры.
-...
+class GameObject:
+    # position = (16, 12) # Начальная позиция (центр) ? 
+
+    def __init__(self, position, body_color):
+        self.position = position
+        self.body_color = body_color
+
+    def draw(self):
+        pass
+
+
+class Apple(GameObject):
+    body_color = (255, 0, 0)
+
+    def __init__(self):
+        self.body_color = Apple.body_color
+        self.position = Apple.randomize_position()
+
+    def randomize_position():
+        position_x = randint(0,32)
+        position_y = randint(0,24)
+        return (position_x, position_y)
+
+    # Метод draw класса Apple
+    # def draw(self):
+    #     rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
+    #     pygame.draw.rect(screen, self.body_color, rect)
+    #     pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+    
+
+
+class Snake(GameObject):
+    pass
 
 
 def main():
